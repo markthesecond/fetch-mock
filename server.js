eb6ff923fc5f65f867ac4f81c55cddd61814cc84
Mark Ritchie <mwritchie2@gmail.com>
2021-09-29 00:49:32 -0500

    commented up server
    Added comments to be more transparent about the functionality.

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
-const express = require('express');
-// const bodyParser = express.json
-const app = express();
+const express = require('express'); // the server package
+const app = express(); // the very base of a server
+// uses a specified port, or defaults to 3000
 const port = process.env.PORT || 3000;
 
+// imports the routes defined in /api/routes.js
 const routes = require('./api/routes');
 
+// sets up express to use read JSON data in req.body
 app.use(express.json({strict: true, type: 'application/json'}));
+/* mounts the routes and appends any therin defined paths under
+    the path `/api/v1` */
 app.use('/api/v1', routes);
 
+// tells server to listen on `port` and run this callback
 app.listen(port, function () {
     console.log(`Points service listenig on port ${port}...`);
 });
