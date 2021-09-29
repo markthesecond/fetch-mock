74c9b0b0c4224fb331a74b5b6d800760b313060a
Mark Ritchie <mwritchie2@gmail.com>
2021-09-29 00:38:38 -0500

    added POST route for points
    Created express route for accepting deposits of points.

--- api/routes.js
+++ api/routes.js
@@ -1,10 +1,22 @@
 const express = require('express');
+const points = require('../controllers/points');
 
+// class for designing modular routes
 const router = express.Router();
 
+// route handlers for the path '/points'
 router.route('/points')
+    // makes GET req's return the point balance
     .get((req,res) => {
         res.json({message: 'Hello!'});
+    })
+    // accepts POST requests and records point transactions
+    .post((req,res) => {
+        // calls createPoints from the points controller
+        points.createPoints(req.body.payer, req.body.points);
+        res.status(201).json(
+            {message: 'Points recorded!'}
+        );
     })
 
 module.exports = router;
